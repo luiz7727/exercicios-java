67f9019c880889f5d40d1ad90e1267c92da542fb
luiz7727 <luiz.antonio999@hotmail.com>
2021-12-11 11:38:22 -0300

    Interface funcionais

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,6 +1,10 @@
 package lambdas;
 
+@FunctionalInterface//Ela faz com que tenha um unico metodo, porque la na main, o java fica perdido em saber qual função executar caso tenha
+//mais de um método
 public interface Calculo {
 
     public abstract double executar(double a, double b);
+    //caso eu coloque outro método, eu não vou conseguir aplicar uma lambda expresion para uma variável.E vai dar erro...
+
 }
