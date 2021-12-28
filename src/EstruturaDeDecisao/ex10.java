0933bfec4e8327be6dacc2e48be83219ee7f0ddc
BlackCoder <blackcoder@pop-os.localdomain>
2021-12-28 15:19:31 -0300

    teste vim

---
+++ src/EstruturaDeDecisao/ex10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex10{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+
+        int number;
+
+        System.out.println("Digite um numero");
+        number = scan.nextInt();
+
+        System.out.println(number);
+    }
+}
